d75b9a1dfac9996f7092a723253da029fa4ed4e3
Dan <danschmit@protonmail.com>
2018-07-21 23:09:00 +0200

    h

--- src/Form/ArticlesType.php
+++ src/Form/ArticlesType.php
@@ -6,6 +6,8 @@ use App\Entity\Articles;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArticlesType extends AbstractType
 {
