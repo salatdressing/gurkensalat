ba8ce25cc879aee9787dd7389b340016e032d62a
Dan <danschmit@protonmail.com>
2018-07-21 22:39:12 +0200

    created index limit by 10

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -10,8 +10,21 @@ class IndexController extends Controller
     /**
      * @Route("/", name="index")
      */
-    public function index()
-    {
-        return $this->render('index/index.html.twig');
-    }
+     public function index()
+     {
+         $article = $this->getDoctrine()
+         ->getRepository(Articles::class)
+         ->getAllOrderDesc(0);
+
+         if (!$article) {
+             throw $this->createNotFoundException(
+           'Keine Artikel vorhanden in der Datenbank'
+         );
+         }
+
+         return $this->render('index/index.html.twig', [
+         'blog_title' => 'Mein Blog',
+         'article' => $article
+       ]);
+     }
 }
