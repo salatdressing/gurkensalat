1e2a0cf25a90b199d4c4f68c4fbb99c157e44ffd
Dan <danschmit@protonmail.com>
2018-07-23 10:57:28 +0200

    use statement for request updated

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use App\Controller\Request;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends Controller
 {
