acdf6332963d7b7d47a37e83d604b91f6e421006
Dan <danschmit@protonmail.com>
2018-07-23 15:30:04 +0200

    t

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -115,7 +115,7 @@ class User implements UserInterface
 
     public function getRoles()
     {
-        $this->roles = $aRole;
+        $aRole = $this->roles;
         return array($aRole);
     }
 
